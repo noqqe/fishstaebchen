1dd7301f09fd9d1c4ae803842853070232e56042
Florian Baumann <flo@noqqe.de>
2018-01-17 14:52:36 +0100

    better colors

--- conf.d/theme.fish
+++ conf.d/theme.fish
@@ -2,24 +2,24 @@
 set fish_bind_mode default
 set fish_color_autosuggestion BD93F9
 set fish_color_cancel -r
-set fish_color_command F8F8F2
+set fish_color_command white
 set fish_color_comment 6272A4
 set fish_color_cwd 6ad2fc
 set fish_color_cwd_root red
 set fish_color_end 50FA7B
-set fish_color_error FFB86C
+set fish_color_error ad6bff
 set fish_color_escape 'bryellow'  '--bold'
 set fish_color_history_current --bold
 set fish_color_host normal
 set fish_color_match --background=brblue
 set fish_color_normal normal
 set fish_color_operator bryellow
-set fish_color_param FF79C6
-set fish_color_quote yellow
+set fish_color_param white
+set fish_color_quote 7de884
 set fish_color_redirection 8BE9FD
 set fish_color_search_match 'bryellow'  '--background=brblack'
 set fish_color_selection 'white'  '--bold'  '--background=brblack'
-set fish_color_status red
+set fish_color_status aa4052
 set fish_color_user a9fc6a
 set fish_color_valid_path underline
 
