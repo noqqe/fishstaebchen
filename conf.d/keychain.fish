fe823e5ae90b3fbd3feb64e6f379743b3a4ea22d
Florian Baumann <flo@noqqe.de>
2018-08-23 07:20:25 +0200

    Dirty hack to get all my keys to keychain

--- conf.d/keychain.fish
+++ conf.d/keychain.fish
@@ -1,4 +1,12 @@
 # This should handle my ssh agent running on this host
 if test -x (which keychain) ; and status --is-interactive;
   [ -e $HOME/.keychain/(hostname)-fish ]; and source $HOME/.keychain/(hostname)-fish
+
+  # Look for all keys and add them to the agent
+
+  set -l keys (grep -l "PRIVATE KEY---" ~/.ssh/*)
+  for key in $keys ;
+    ssh-add $key ^/dev/null
+  end
+
 end
