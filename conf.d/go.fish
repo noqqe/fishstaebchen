25bd3c050ab1d7f42996475c96e9fb3541e3ec3b
Florian Baumann <flo@noqqe.de>
2018-11-01 22:07:17 +0100

    new plugins

---
+++ conf.d/go.fish
@@ -0,0 +1,12 @@
+# golang system and directory settings if installed
+
+set -l gopath (which go 2>/dev/null)
+set -l gohome $HOME/.go/ # thats so nice. go home. thihihi.
+
+if test -x $gopath
+  if not test -d $gohome
+    mkdir $gohome
+  end
+  set -x GOPATH $gohome
+  set PATH $PATH $gohome/bin
+end
