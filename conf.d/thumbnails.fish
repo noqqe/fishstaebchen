8105bf3ac5bceaefbfe4523506b345aa43466f2b
Florian Baumann <flo@noqqe.de>
2018-11-02 09:04:11 +0100

    fix

--- conf.d/thumbnails.fish
+++ conf.d/thumbnails.fish
@@ -1,7 +1,7 @@
 # Create a thumbnail image for all pictures in current folder
 function create_thumbs ()
   for x in *.jpg
-    set -l tn_name (echo $x | sed 's/\.jpg/_thumb.jpg/')
+    set -l tn_name (echo $x | sed 's/\.jpg/-thumb.jpg/')
     convert $x -define jpeg:size=300x300 -thumbnail '300x300>' $tn_name
   end
 end
