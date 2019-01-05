e5bcc6b23704a6fa9544b77a6f2f0a42e01c955c
Florian Baumann <flo@noqqe.de>
2019-01-05 11:37:13 +0100

    full path in multiline prompt

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -64,5 +64,5 @@ function fish_prompt --description 'Write out the prompt'
     end
 
 
-    echo -n -s (set_color yellow) (date "+%F %H:%M ") (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status \n $suffix " "
+    echo -n -s (set_color yellow) (date "+%F %H:%M ") (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) $PWD $normal (__fish_git_prompt) $normal $prompt_status \n $suffix " "
 end
