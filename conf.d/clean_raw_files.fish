17f99afed804ce2f0f851142e8750893d4921dfe
Florian Baumann <flo@noqqe.de>
2019-01-27 15:24:04 +0100

    Sanity checks for clean_raw_files.fish

--- conf.d/clean_raw_files.fish
+++ conf.d/clean_raw_files.fish
@@ -1,6 +1,17 @@
 function clean_raw_files
+
+  if test (count *.ARW) -lt 1
+    echo "No ARW files found in this dir. You sure you are right here?"
+    return
+  end
+
   # Get directory with source JPGs
   set ORIG_DIR $argv[1]
+
+  if test (count $ORIG_DIR/*.JPG) -lt 1 2>/dev/null
+    echo "No JPG files found in ORIG_DIR. You sure you gave the right path?"
+    return
+  end
 
   # Get list of all files in current dir
   set ORIG_FILES (ls .)
@@ -12,7 +23,7 @@ function clean_raw_files
 
     if not test -f {$ORIG_DIR}/{$FILENAME}.JPG
       echo "Did not find $FILENAME.JPG. Deleting $FILENAME.ARW"
-      rm $FILENAME.ARW
+      echo rm $FILENAME.ARW
     end
   end
 end
