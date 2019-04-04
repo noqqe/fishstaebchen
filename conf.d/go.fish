2dff2af3574b2231be6d607bcb5d644efe60ccdd
Florian Baumann <flo@noqqe.de>
2019-04-04 09:30:42 +0200

    Update go.fish

--- conf.d/go.fish
+++ conf.d/go.fish
@@ -3,8 +3,8 @@
 set -l gopath (which go 2>/dev/null)
 set -l gohome $HOME/.go/ # thats so nice. go home. thihihi.
 
-if test -x $gopath
-  if not test -d $gohome
+if test -x "$gopath"
+  if not test -d "$gohome"
     mkdir $gohome
   end
   set -x GOPATH $gohome
