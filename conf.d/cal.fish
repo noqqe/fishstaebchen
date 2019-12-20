f8462f475ce5678c634df6a61405d7a171222327
noqqe <noqqe@nostra.intern.k4cg.org>
2019-12-20 21:41:21 +0100

    stop using bat

--- conf.d/cal.fish
+++ conf.d/cal.fish
@@ -1,7 +1,7 @@
 # Add calendar shortcut when gcal is installed
 # Finally something to show week numbers
 #
-set CAL (which gcal)
+set CAL (which gcal 2>/dev/null)
 
 if test -x $CAL
   alias cal "gcal -K -s Mo .+"
--- conf.d/cat.fish
+++
@@ -1,8 +0,0 @@
-# Add calendar shortcut when gcal is installed
-# Finally something to show week numbers
-#
-set BAT (which bat)
-
-if test -x "$BAT"
-  alias cat $BAT
-end
