0da94d6b868b1acc017d22bce6c9443a01b44835
Florian Baumann <flo@noqqe.de>
2021-08-24 08:26:55 +0200

    exa hidden

--- conf.d/exa.fish
+++ conf.d/exa.fish
@@ -4,8 +4,8 @@
 set EXA (which exa 2>/dev/null)
 
 if test -x "$EXA"
-  alias l='exa -lh --git --time-style long-iso'
+  alias l='exa -lh --git --time-style long-iso --all'
   alias ls='exa'
-  alias tree='exa -lh --git --time-style long-iso --tree'
-  alias lr='exa -lh --git --time-style long-iso --tree'
+  alias tree='exa -lh --git --time-style long-iso --tree --all'
+  alias lr='exa -lh --git --time-style long-iso --tree --all'
 end
