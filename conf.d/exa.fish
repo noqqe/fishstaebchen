f71f5e61107ef95a1fa318ce16ac2196c4ce135c
Florian Baumann <flo@noqqe.de>
2023-09-07 13:04:24 +0200

    replace exa with eza since its orphaned

--- conf.d/exa.fish
+++ conf.d/exa.fish
@@ -1,11 +1,10 @@
 # Drop in replacement for ls
 # Not sure if i like it so far, but i think i give it a try
 
-
-if command -q exa
-  alias l='exa -lh --git --time-style long-iso --all'
-  alias ls='exa'
-  alias tree='exa -lh --git --time-style long-iso --tree --all'
-  alias lr='exa -lh --git --time-style long-iso --tree --all'
-  alias lslahtr='exa -lh --all --git --time-style long-iso --sort newest'
+if command -q eza
+  alias l='eza -lh --git --time-style long-iso --all'
+  alias ls='eza'
+  alias tree='eza -lh --git --time-style long-iso --tree --all'
+  alias lr='eza -lh --git --time-style long-iso --tree --all'
+  alias lslahtr='eza -lh --all --git --time-style long-iso --sort newest'
 end
