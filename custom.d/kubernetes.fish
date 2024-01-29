6a4bd070e96a4ddffc203926afd7106b04a7b56b
Florian Baumann <flo@noqqe.de>
2024-01-29 09:56:47 +0100

    Add kubernetes aliases

---
+++ custom.d/kubernetes.fish
@@ -0,0 +1,9 @@
+# Aliases for kubernetes 
+
+if command -q kubectl; 
+  alias k=kubectl
+end
+
+if command -q helm; 
+  alias h=helm
+end
--- custom.d/update.fish
+++
@@ -1,6 +0,0 @@
-function update
-  if not test -r ~/.config/fish/functions/fisher.fish
-    curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-  end
-  fisher
-end
