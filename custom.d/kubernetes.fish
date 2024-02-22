e64469c69f30e3fbc7d2ec6c58549d7c8bedb5fe
Florian Baumann <flo@noqqe.de>
2024-02-22 10:36:51 +0100

    add 2 kubernetes shortcuts

--- custom.d/kubernetes.fish
+++ custom.d/kubernetes.fish
@@ -2,8 +2,25 @@
 
 if command -q kubectl; 
   alias k=kubectl
+
 end
 
 if command -q helm; 
   alias h=helm
+end
+
+
+function k_get_pod_ports
+  for pod in $argv
+    echo Pod $pod:
+    kubectl get pod $pod -o jsonpath='Host:{.status.hostIP} PodIP:{.status.podIP} PodPorts:{.spec.containers[*].ports[*].containerPort}'
+    echo
+  end
+end
+
+function k_get_secret
+  for secret in $argv
+    echo Secret $secret:
+    kubectl get secret $secret -o go-template='{{range $k,$v := .data}}{{printf " %s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}'
+  end
 end
