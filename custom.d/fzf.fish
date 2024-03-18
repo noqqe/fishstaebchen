830035767a692b2141681c4f2e8a5771115e584e
Florian Baumann <flo@noqqe.de>
2024-03-18 16:14:03 +0100

    Add fzf completion

--- custom.d/fzf.fish
+++ custom.d/fzf.fish
@@ -1,2 +1,8 @@
-set -Ux FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
+# Colorscheme: dracula
+set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
+# Borders
+set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --border=\"none\" --border-label=\"\" --preview-window=\"border-rounded\""
+
+# Height
+set -Ux FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --height=10"
