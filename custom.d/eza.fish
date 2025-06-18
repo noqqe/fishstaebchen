a317daf2da8b5567c354eb9035ab9d49009be1d9
Florian Baumann <flo@noqqe.de>
2025-06-18 11:07:39 +0200

    add lstr as tree replacement

--- custom.d/eza.fish
+++ custom.d/eza.fish
@@ -4,7 +4,4 @@
 if command -q eza
   alias l='eza -lh --git --time-style long-iso --all --icons auto'
   alias ls='eza'
-  alias tree='eza -lh --git --time-style long-iso --tree --all --icons auto'
-  alias lr='eza -lh --git --time-style long-iso --tree --all --icons auto'
-  alias lslahtr='eza -lh --all --git --time-style long-iso --sort newest --icons auto'
 end
---
+++ custom.d/lstr.fish
@@ -0,0 +1,5 @@
+# https://github.com/bgreenwell/lstr
+if command -q lstr
+   alias treei="lstr interactive -gGs --icons"
+   alias tree="lstr -gGs --icons"
+end
