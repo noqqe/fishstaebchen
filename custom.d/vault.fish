e661170ca056d8c2c46696e978ba9862715c8b62
Florian Baumann <florian.baumann@yougov.com>
2026-02-12 11:11:05 +0100

    Enhance vault_show_secret function

--- custom.d/vault.fish
+++ custom.d/vault.fish
@@ -3,6 +3,13 @@ function vault_show_secret
     echo "Not logged in"
     return 1
   end 
+
+  if test (count $argv) -lt 1
+    echo "Usage: vault_show_secret <path>"
+    return 1
+  end 
+
   set secret (vault kv list -format=json $argv[1] | jq -r .[] | fzf)
+  printf "# %s/%s\n" $argv[1] $secret
   vault kv get -format json $argv[1]/$secret | jq .data.data
 end 
