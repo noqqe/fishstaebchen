bb389ba04528a603cdcd0a21ae525d4adbf2f151
Florian Baumann <florian.baumann@yougov.com>
2026-02-13 07:50:39 +0100

    Time to leave ag alias

--- custom.d/rg.fish
+++
@@ -1,7 +0,0 @@
-# I switched to ripgrep, but for convenience i just use ag
-# command anyways.
-
-if command -q rg
-  alias srg="rg --no-line-number --no-filename --no-multiline --no-heading"
-  alias ag=rg
-end
