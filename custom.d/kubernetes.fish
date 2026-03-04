08cebaaf83bab13206089548950c89f28ad3fe04
Florian Baumann <florian.baumann@yougov.com>
2026-03-04 09:57:45 +0100

    Dynamic secret for kubernetes using fzf

--- custom.d/kubernetes.fish
+++ custom.d/kubernetes.fish
@@ -13,7 +13,7 @@ if command -q helm;
 end
 
 
-function k_get_pod_ports
+function kubernetes_show_pod_ports
   for pod in $argv
     echo Pod $pod:
     kubectl get pod $pod -o jsonpath='Host:{.status.hostIP} PodIP:{.status.podIP} PodPorts:{.spec.containers[*].ports[*].containerPort}'
@@ -21,9 +21,13 @@ function k_get_pod_ports
   end
 end
 
-function k_get_secret
-  for secret in $argv
-    echo Secret $secret:
-    kubectl get secret $secret -o go-template='{{range $k,$v := .data}}{{printf " %s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}'
+function kubernetes_show_secrets
+  if not kubectl auth can-i get secrets &>/dev/null
+    echo "You don't have permissions to view secrets"
+    return 1
   end
+
+  set -l secret (kubectl get secrets -o json | jq -r .items[].metadata.name | fzf)
+  echo Secret $secret:
+  kubectl get secret $secret -o go-template='{{range $k,$v := .data}}{{printf " %s: " $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}'
 end
