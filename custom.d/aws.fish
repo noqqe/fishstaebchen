f049eb6e90cd7044b2a0d730f95585ebc12156a6
Florian Baumann <florian.baumann@yougov.com>
2026-05-19 14:38:13 +0200

    Add aws ssm fetcher

--- custom.d/aws.fish
+++ custom.d/aws.fish
@@ -12,3 +12,18 @@ function aws_show_secret
     echo $value 
   end 
 end
+
+# my tiny aws ssm fetcher
+function aws_show_ssm
+  if not aws sts get-caller-identity &>/dev/null
+    echo "Not logged in"
+    return 1
+  end
+  set name (aws ssm describe-parameters | jq -r .Parameters[].Name | fzf)
+  set value "$(aws ssm get-parameter --with-decryption --name $name |jq -r '.Parameter | {Name,Value}')"
+  if echo $value | jq -e . &>/dev/null 
+    echo $value | jq -r '.'
+  else
+    echo $value 
+  end 
+end
